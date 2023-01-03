034d6e401e8428f437fdef210060d8e3e3a94757
GargDivanshu <divanshu.garg113@gmail.com>
2023-01-03 22:30:28 +0530

    basic

---
+++ app.js
@@ -0,0 +1,34 @@
+const mongoose  = require('mongoose');
+const express = require('express');
+const app = express();
+mongoose.connect("mongodb://localhost:27017/ahvan/sports", {useNewUrlParser: true}).then(() => {
+    console.log(`CONNECTED TO MONGO!`);
+})
+.catch((err) => {
+    console.log(`OH NO! MONGO CONNECTION ERROR!`);
+    console.log(err);
+});
+
+const sportsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+})
+
+
+
+
+// const Badminton = mongoose.model("Badminton", sportsSchema);
+
+
+
+app.get('/sports/:id', async (req, res) => {
+    const { id } = req.params;
+    const findSports = await Sports.findById(id);
+    res.send(findSports._id)
+})
+
+app.listen(3000, () => {
+    console.log('Server started');
+})
